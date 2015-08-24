f11fdca7ad953028809f09e893129eed63465d7c
Matheus Lisboa de Barros <matheuslbarros@gmail.com>
2015-08-24 15:24:36 -0300

    Abstract Model

---
+++ UniRitter.UniRitter2015/Models/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniRitter.UniRitter2015.Models
+{
+    abstract public class Model : IModel
+    {
+        public Guid? id { get; set; }
+
+        abstract public bool Equals(Model other);
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null)
+            {
+                return Equals(obj as Model);
+            }
+            return false;
+        }
+        
+        public override int GetHashCode()
+        {
+            return id.GetHashCode();
+        }
+    }
+}
